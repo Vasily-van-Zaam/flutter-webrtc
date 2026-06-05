50c11892285e8600c056932d376673536c7d3373
Vvan <vvan@synergy.ru>
2026-06-05 10:42:47 +0300

    chore(windows): log native audio options

--- common/cpp/src/flutter_media_stream.cc
+++ common/cpp/src/flutter_media_stream.cc
@@ -2,6 +2,8 @@
 
 #include "flutter_utf8_sanitize.h"
 
+#include <iostream>
+
 #define DEFAULT_WIDTH 1280
 #define DEFAULT_HEIGHT 720
 #define DEFAULT_FPS 30
@@ -175,6 +177,17 @@ std::string getDeviceIdConstraint(const EncodableMap& mediaConstraints) {
   return "";
 }
 
+void logNativeAudioOptions(const RTCAudioOptions& options) {
+  std::cout << "[AudioConstraints] native GetUserAudio "
+            << "AGC=" << (options.auto_gain_control ? "true" : "false")
+            << " EchoCancellation="
+            << (options.echo_cancellation ? "true" : "false")
+            << " NoiseSuppression="
+            << (options.noise_suppression ? "true" : "false")
+            << " HighpassFilter="
+            << (options.highpass_filter ? "true" : "false") << std::endl;
+}
+
 void FlutterMediaStream::GetUserAudio(const EncodableMap& constraints,
                                       scoped_refptr<RTCMediaStream> stream,
                                       EncodableMap& params) {
@@ -257,6 +270,7 @@ void FlutterMediaStream::GetUserAudio(const EncodableMap& constraints,
       }
     }
 
+    logNativeAudioOptions(audio_options);
     scoped_refptr<RTCAudioSource> source = base_->factory_->CreateAudioSource(
         "audio_input", RTCAudioSource::SourceType::kMicrophone, audio_options);
     std::string uuid = base_->GenerateUUID();
